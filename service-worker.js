d0e6118d3f98a691624116fa675644fbfaa61513
Vaios Anastasiadis <vaios.anastasiadis@agileactors.com>
2026-05-19 18:31:59 +0300

    Fix

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,33 @@
-const CACHE_NAME = "chord-trainer-v1";
+const CACHE_NAME = "chord-trainer-v2";
 
 const urlsToCache = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => response || fetch(event.request)),
+    fetch(event.request).catch(() => caches.match(event.request)),
   );
 });
